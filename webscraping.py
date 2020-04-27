01b2de926aea919e8b0cf325962717b77bee4766
David Funes <davidgfunes@gmail.com>
2020-04-26 21:47:39 -0300

    webscraping.py updated: changed the source of the frame data. This should make the extraction easier.

--- webscraping.py
+++ webscraping.py
@@ -1,15 +1,13 @@
 '''Viejo, no entiendo una goma'''
-
 import bs4
 import requests
 
-
-response = requests.get("https://www.dustloop.com/wiki/index.php?title=GBVS/Gran")
+response = requests.get("https://www.dustloop.com/wiki/index.php?title=GBVS/Gran/Frame_Data")
 
 if response is not None:
     html = bs4.BeautifulSoup(response.text, 'html.parser')
 
-    title = html.select("#Normal_Moves")[0].text
+    title = html.select("#Health")[0].text
     paragraphs = html.select("tr")
     for para in paragraphs:
         print (para.text)
@@ -17,3 +15,5 @@ if response is not None:
     # just grab the text up to contents as stated in question
     intro = '\n'.join([ para.text for para in paragraphs[0:5]])
     print (intro)
+
+# class wikitable
